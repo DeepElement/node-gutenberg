3302228ddbdc103b785472cec31612c795d0add8
Todd Morrison <todd@deepelement.com>
2014-10-07 07:42:25 -0700

    FTP Based API - prepping for v1.0

--- bin/gutenberg.js
+++ bin/gutenberg.js
@@ -1,141 +1,355 @@
-var request = require('request'),
-	async = require('async'),
-	AdmZip = require('adm-zip'),
-	fs = require('fs'),
-	path = require('path'),
-	mkdirp = require('mkdirp'),
-	http = require('http'),
-	libxmljs = require("libxmljs");
+var async = require('async'),
+    fs = require('fs'),
+    path = require('path'),
+    mkdirp = require('mkdirp'),
+    temp = require("temp").track(),
+    Download = require('download'),
+    tar = require('tar-stream'),
+    parseString = require('xml2js').parseString,
+    util = require('util'),
+    ftp = require('jsftp');
 
 // Constructor
 var gutenberg = function(options) {
-	this.options = options || {};
-	this._rdfZipFile = this.options.rdfZipFile;
-	this._rsyncDir = this.options.catalogDir;
-}
+    this.options = options || {};
+    this._ftpClient = new ftp({
+        host: 'mirrors.xmission.com',
+        debugMode: true,
+        port: 21
+    });
+    this._catalogueAddress = this.options.catalogue || 'http://gutenberg.readingroo.ms/cache/generated/feeds/rdf-files.tar.zip';
+    this._masterCataloguePath = null;
+};
 
-gutenberg.prototype.getCatalogueItemByKey = function(key, callback) {
-	var keyClean = key.replace('etext', '');
-	var documentRoot = this._rsyncDir;
-	if (keyClean.length > 0) {
-		documentRoot = path.join(documentRoot, keyClean[0]);
-	}
-	if (keyClean.length > 1) {
-		documentRoot = path.join(documentRoot, keyClean[1]);
-	}
+gutenberg.prototype.getCatalogueMetadata = function(data, callback) {
+    var self = this;
+    var results = [];
+    var maxImported = data.maxImported || 99999;
 
-	if (keyClean.length > 2) {
-		documentRoot = path.join(documentRoot, keyClean[2]);
-	}
+    this._hydrateCatalogue({}, function(err, catalogueFilePath) {
+        if (err)
+            return callback(err);
 
-	if (keyClean.length > 3) {
-		documentRoot = path.join(documentRoot, keyClean[3]);
-	}
+        var extract = tar.extract();
+        extract.on('entry', function(header, stream, extractCallback) {
+            if (results.length >= maxImported) {
+                extract.destroy();
+                return;
+            }
 
-	var documentRoot = path.join(documentRoot, keyClean);
-	var contentZipFile = path.join(documentRoot, keyClean + ".zip");
-	try {
-		var zip = new AdmZip(contentZipFile);
-		var data = zip.readAsText(keyClean + ".txt");
-		this._ContentParseRecord(
-			data, callback);
-	} catch (ex) {
-		return callback(ex);
-	}
-}
+            var content = "";
+            stream.on("data", function(d) {
+                content += d.toString();
+            });
 
-// Parms: format
-gutenberg.prototype.catalogueGetRecords = function(options, callback) {
-	var self = this;
-	self._CatalogueParseRecords(function(err, records) {
-		return callback(err, records);
-	});
-}
+            stream.on('end', function() {
+                if (results.length < maxImported) {
+                    gutenberg.prototype._parseRecord(content,
+                        function(err, resp) {
+                            if (err) {
+                                return extractCallback(err);
+                            }
+                            if (resp.title && resp.formats.length > 0)
+                                results.push(resp);
+                            return extractCallback();
+                        });
+                } else
+                    return extractCallback();
+            });
+
+            stream.resume();
+        });
+
+        extract.on('close', function(err) {
+            return callback(null, results);
+        });
+
+        extract.on('error', function(err) {
+            return callback(err);
+        });
+
+        extract.on('finish', function(err) {
+            return callback(err, results);
+        });
+
+        fs.createReadStream(catalogueFilePath).pipe(extract);
+    });
+};
+
+gutenberg.prototype.getCatalogueMetadataById = function(id, callback) {
+    var self = this;
+    var results = [];
+    var result = null;
+
+    this._hydrateCatalogue({}, function(err, catalogueFilePath) {
+        if (err)
+            return callback(err);
+
+        var extract = tar.extract();
+        extract.on('entry', function(header, stream, extractCallback) {
+            if (result) {
+                extract.destroy();
+                return;
+            }
+
+            var content = "";
+            stream.on("data", function(d) {
+                content += d.toString();
+            });
+
+            stream.on('end', function() {
+                if (!result) {
+                    gutenberg.prototype._parseRecord(content,
+                        function(err, resp) {
+                            if (err) {
+                                return extractCallback(err);
+                            }
+                            if (resp.id == id)
+                                result = resp;
+                            return extractCallback();
+                        });
+                } else
+                    return extractCallback();
+            });
+
+            stream.resume();
+        });
+
+        extract.on('close', function(err) {
+            return callback(null, result);
+        });
+
+        extract.on('error', function(err) {
+            return callback(err);
+        });
+
+        extract.on('finish', function(err) {
+            return callback(err, result);
+        });
+
+        fs.createReadStream(catalogueFilePath).pipe(extract);
+    });
+};
+
+
+gutenberg.prototype._parseRecord = function(record, callback) {
+    var response = {
+        id: null,
+        formats: [],
+        downloads: 0,
+        languages: [],
+        title: null,
+        author: null,
+        isTextAvailable: false,
+        issued: null
+    };
+
+    parseString(record, function(err, jsonRecord) {
+        if (err)
+            return callback(err);
+
+        try {
+            response.id = jsonRecord['rdf:RDF']['pgterms:ebook'][0]['$']['rdf:about'].replace('ebooks/', '');
+            if (jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:language'][0]) {
+                if (jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:language'][0]['rdf:Description']) {
+                    response.languages.push(jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:language'][0]['rdf:Description'][0]['rdf:value'][0]["_"]);
+                } else {
+                    response.languages.push(jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:language'][0]["_"]);
+                }
+            }
+            if (jsonRecord['rdf:RDF']['pgterms:ebook'][0]['pgterms:downloads']) {
+                if (jsonRecord['rdf:RDF']['pgterms:ebook'][0]['pgterms:downloads'][0]["rdf:value"]) {
+                    response.downloads = jsonRecord['rdf:RDF']['pgterms:ebook'][0]['pgterms:downloads'][0]["rdf:value"][0]["_"];
+                } else {
+                    response.downloads = jsonRecord['rdf:RDF']['pgterms:ebook'][0]['pgterms:downloads'][0]["_"];
+                }
+            }
+
+            if (jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:title']) {
+                response.title = jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:title'];
+            }
+
+
+            var creatorNode = null;
+
+            if (jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:creator']) {
+                creatorNode = jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:creator'];
+            } else {
+                var creatorParent = jsonRecord['rdf:RDF']['pgterms:ebook'][0];
+                var marcrelKeys = [];
+                for (var key in creatorParent) {
+                    if (key.indexOf('marcrel:') > -1) {
+                        marcrelKeys.push(key);
+                    }
+                }
+                if (marcrelKeys.length > 0) {
+                    creatorNode = jsonRecord['rdf:RDF']['pgterms:ebook'][0][marcrelKeys[0]];
+                }
+            }
+
+            if (creatorNode) {
+                response.author = creatorNode[0]['pgterms:agent'][0]['pgterms:name'][0];
+            } else {
+                response.author = 'Unknown';
+            }
+
+            if (jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:issued']) {
+                response.issued = jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:issued'][0]['_'];
+            }
+
+            if (jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:hasFormat']) {
+                var files = jsonRecord['rdf:RDF']['pgterms:ebook'][0]['dcterms:hasFormat'];
+                for (var i = 0; i <= files.length - 1; i++) {
+                    var file = files[i];
+                    var format = file['pgterms:file'][0]['dcterms:format'][0]['rdf:Description'][0]['rdf:value'][0]['_'];
+                    var tempFilePath = file['pgterms:file'][0]['$']['rdf:about'];
+                    var fileName = file['pgterms:file'][0]["$"]["rdf:about"].replace('http://www.gutenberg.org/ebooks/', '').replace('http://www.gutenberg.org/files/', '');
+
+                    var outputContainer = {
+                        format: format,
+                        fileName: fileName,
+                        extension: fileName.substr(fileName.lastIndexOf('.') + 1)
+                    };
+
+                    if (tempFilePath.indexOf('http://www.gutenberg.org/dirs') > -1)
+                        outputContainer.specialPath = tempFilePath.replace('http://www.gutenberg.org/dirs', '');
+
+                    response.formats.push(outputContainer);
+                }
+            }
+            return callback(null, response);
+        } catch (ex) {;
+            return callback(ex, response);
+        }
+    });
+};
+
+gutenberg.prototype._hydrateCatalogue = function(data, callback) {
+    var self = this;
+    if (!self._masterCataloguePath) {
+        temp.mkdir('gutenberg', function(err, dirPath) {
+            if (err)
+                return callback(new Error(err));
+
+            var download = new Download()
+                .get(self._catalogueAddress, dirPath, {
+                    extract: true,
+                    strip: 1
+                });
+            download.run(function(err, files, stream) {
+                if (err)
+                    return callback(new Error(err));
+                self._masterCataloguePath = path.join(dirPath, 'rdf-files.tar');
+                return callback(null, self._masterCataloguePath);
+            });
+        });
+    } else
+        return callback(null, self._masterCataloguePath);
+};
+
+
+gutenberg.prototype.getCatalogueItemByKey = function(data, callback) {
+    var id = data.id;
+    var extension = data.extension;
+    var _self = this;
+    _self.getCatalogueMetadataById(id,
+        function(err, item) {
+            if (err)
+                return callback(err);
+            var formatList = item.formats.filter(function(f) {
+                if (f.extension == extension)
+                    return true;
+                return false;
+            });
+
+            if (formatList.length > 0) {
+                var formatFile = formatList[0];
+                var documentRoot = 'gutenberg/';
+
+                var contentFile;
+                if (formatFile.specialPath) {
+                    contentFile = path.join(documentRoot, formatFile.specialPath);
+
+                } else {
+                    if (id.length > 0) {
+                        documentRoot = path.join(documentRoot, id[0]);
+                    }
+                    if (id.length > 1) {
+                        documentRoot = path.join(documentRoot, id[1]);
+                    }
+
+                    if (id.length > 2) {
+                        documentRoot = path.join(documentRoot, id[2]);
+                    }
+
+                    if (id.length > 3) {
+                        documentRoot = path.join(documentRoot, id[3]);
+                    }
+
+                    contentFile = path.join(documentRoot, id, id + "." + formatFile.extension);
+                }
+
+                if (contentFile) {
+                    var content = "";
+
+                    console.log(contentFile);
+                    _self._ftpClient.get(contentFile, function(err, socket) {
+                        if (err) {
+                            console.log({
+                                err: err,
+                                file: contentFile
+                            });
+                            return callback(err);
+                        }
+
+                        socket.on("data", function(d) {
+                            content += d.toString();
+                        });
+                        socket.on("close", function(err) {
+                            if (err)
+                                return callback(err);
+                            gutenberg.prototype._ContentParseRecord(
+                                content, callback);
+                        });
+                        socket.resume();
+                    });
+                } else
+                    return callback('format-not-found');
+
+            } else
+                return callback('format-not-found');
+        });
+};
 
 gutenberg.prototype._ContentParseRecord = function(content, callback) {
+    var indexOfHeaderStart = content.indexOf("*** START OF");
+    var indexOfHeaderEnd = content.indexOf('\n', indexOfHeaderStart);
+    var header = content.substr(0, indexOfHeaderEnd);
+    var indexOfFooterStart = content.indexOf("*** END OF");
+    var footer = content.substr(indexOfFooterStart, content.length);
+    var cleanContent = content.substr(indexOfHeaderEnd, indexOfFooterStart - indexOfHeaderEnd);
 
-	var indexOfHeaderStart = content.indexOf("*** START OF THIS PROJECT GUTENBERG");
-	var indexOfHeaderEnd = content.indexOf('\n', indexOfHeaderStart);
-	var header = content.substr(0, indexOfHeaderEnd);
+    var result = {
+        header: header,
+        footer: footer,
+        content: cleanContent
+    };
 
-	var indexOfFooterStart = content.indexOf("End of the Project Gutenberg");
-	var footer = content.substr(indexOfFooterStart, content.length);
-
-	var cleanContent = content.substr(indexOfHeaderEnd, indexOfFooterStart - indexOfHeaderEnd);
-
-	var indexAuthorStart = header.indexOf('Author:');
-	var indexAuthorEnd = header.indexOf('\n', indexAuthorStart);
-	var authorsList = header.substr(indexAuthorStart, indexAuthorEnd - indexAuthorStart).replace("Author:", "").trim().split('and');
-	var authors = [];
-	authorsList.forEach(function(a) {
-		authors.push(a.trim());
-	});
-
-	var indexOfTitleStart = header.indexOf("Title:");
-	var indexOfTitleEnd = header.indexOf('\n', indexOfTitleStart);
-	var title = header.substr(indexOfTitleStart, indexOfTitleEnd - indexOfTitleStart).replace("Title:", "").trim();
-
-	var indexOfLanguageStart = header.indexOf("Language:");
-	var indexOfLanguageEnd = header.indexOf('\n', indexOfLanguageStart);
-	var language = header.substr(indexOfLanguageStart, indexOfLanguageEnd - indexOfLanguageStart).replace("Language:", "").trim();
-
-	var result = {
-		title: title,
-		language: language,
-		authors: authors,
-		content : cleanContent
-	};
-	return callback(null, result);
-}
-
-gutenberg.prototype._CatalogueParseRecords = function(callback) {
-	var results = [];
-
-	var zip = new AdmZip(this._rdfZipFile);
-	var data = zip.readAsText("catalog.rdf");
-
-	var startMatch = "<pgterms:etext";
-	var endMatch = "</pgterms:etext>";
-	var startIndex = 0;
-	while (startIndex > -1) {
-		startIndex = data.indexOf(startMatch, startIndex);
-		if (startIndex > -1) {
-			var endIndex = data.indexOf(endMatch, startIndex + startMatch.length);
-			var fragment = data.substr(startIndex, endIndex - startIndex + endMatch.length);
-			var parsed = this._parseRecord(fragment);
-			startIndex = endIndex + endMatch.length;
-			results.push(parsed);
-		}
-	}
-
-	return callback(null, results);
-}
-
-
-gutenberg.prototype._parseRecord = function(record) {
-	var response = {};
-	try {
-		var cleanRecord = record.replace(/dc:/g, '').replace(/pgterms:/g, '').replace(/rdf:/g, '').replace(/xsd:/g, '').replace(/&lic;/g, 'public').replace(/dcterms:/g, '').replace(/&pg;/g, 'gutenberg');
-
-		var xmlDoc = libxmljs.parseXml(cleanRecord);
-		response.id = xmlDoc.root().attr('ID').value();
-		xmlDoc.root().childNodes().forEach(function(c) {
-			if (c.name() != null && c.name() != 'text')
-				response[c.name()] = c.text();
-		});
-	} catch (ex) {
-		console.log("not able to parse fragment");
-	}
-	return response;
-}
+    var result = {
+        header: header,
+        footer: footer,
+        content: cleanContent
+    };
+    return callback(null, result);
+};
 
 // export the class
 if (typeof exports === 'object') {
-	module.exports = gutenberg;
+    module.exports = gutenberg;
 } else if (typeof define === 'function' && define.amd) {
-	define(function() {
-		return gutenberg;
-	});
+    define(function() {
+        return gutenberg;
+    });
 } else {
-	this.gutenberg = gutenberg;
-}
+    this.gutenberg = gutenberg;
+};
--- test/integration-gutenberg-tests.js
+++ test/integration-gutenberg-tests.js
@@ -1,70 +1,173 @@
 var util = require('./test'),
-	gutenberg = require('../bin/gutenberg'),
-	should = require('should'),
-	path = require('path'),
-	async = require('async');
+    gutenberg = require('../bin/gutenberg'),
+    should = require('should'),
+    path = require('path'),
+    async = require('async'),
+    fs = require('fs');
 
 describe('api integration', function() {
-	describe('getCatalogueItemByKey', function() {
+    describe('getCatalogueMetadata', function() {
+        var instance;
+        var sampleKeys = [];
 
-		var instance;
-		var sampleIds = ['etext10001', 'etext10002', 'etext10003', 'etext10004', 'etext10005', 'etext10006', 'etext10007', 'etext10008', 'etext10009'];
-		var sampleRecords;
+        it('Standard Success', function(done) {
+            this.timeout(999999);
+            instance = new gutenberg();
+            instance.getCatalogueMetadata({
+                    maxImported: 11000
+                },
+                function(err, keysResp) {
+                    if (err)
+                        return done(err);
+                    should.exist(keysResp);
+                    keysResp.length.should.be.above(0);
+                    keysResp.forEach(function(r) {
+                        should.exist(r.id);
+                        should.exist(r.formats);
+                        should.exist(r.languages);
+                        should.exist(r.title);
+                        should.exist(r.author);
+                        should.exist(r.downloads);
+                        should.exist(r.issued);
 
-		beforeEach(function(done) {
-			instance = new gutenberg({
-				catalogDir: path.join(__dirname, "/samples/catalog"),
-				rdfZipFile: path.join(__dirname, "/samples/gutenberg.rdf.zip")
-			});
+                        r.formats.length.should.be.above(0);
+                        r.languages.length.should.be.above(0);
+                    });
+                    return done();
+                });
+        });
 
-			instance.catalogueGetRecords({},
-				function(err, resp) {
-					if (err)
-						done(err);
+        it('MaxImported Count', function(done) {
+            this.timeout(999999);
+            instance = new gutenberg();
+            var maxImported = 5;
+            instance.getCatalogueMetadata({
+                    maxImported: maxImported
+                },
+                function(err, keysResp) {
+                    if (err)
+                        return done(err);
+                    keysResp.length.should.equal(maxImported);
+                    return done();
+                });
+        });
+    });
 
-					sampleRecords = resp.filter(function(r) {
-						if (sampleIds.indexOf(r.id) > -1)
-							return true;
-						return false;
-					});
-					done();
-				});
-		});
+    describe('getCatalogueMetadataById', function() {
+        var instance;
+        var sampleMetadata = [];
 
-		it('Validate catalogue fetch', function(done) {
-			async.eachSeries(sampleRecords,
-				function(item, item_callback) {
-					instance.getCatalogueItemByKey(
-						item.id,
-						function(err, data) {
-							should.not.exist(err);
-							should.exist(data);
-							should.exist(data.content);
-							item_callback(err);
-						});
-				},
-				function(err) {
-					done(err);
-				});
-		});
-	});
+        beforeEach(function(done) {
+            instance = new gutenberg();
+            instance.getCatalogueMetadata({
+                    maxImported: 5
+                },
+                function(err, keysResp) {
+                    if (err)
+                        return done(err);
+                    sampleMetadata = keysResp;
+                    return done();
+                });
+        });
 
-	describe('catalogueGetRecords', function() {
-		it('Validate download integrity', function(done) {
-			this.timeout(999999);
-			var instance = new gutenberg({
-				catalogDir: path.join(__dirname, "/samples/catalog"),
-				rdfZipFile: path.join(__dirname, "/samples/gutenberg.rdf.zip")
-			});
-			instance.catalogueGetRecords({},
-				function(err, resp) {
-					if (err)
-						done(err);
+        it('Standard Success', function(done) {
+            this.timeout(999999);
+            async.each(sampleMetadata,
+                function(meta, metaCb) {
+                    instance = new gutenberg();
+                    instance.getCatalogueMetadataById(
+                        meta.id,
+                        function(err, r) {
+                            if (err)
+                                return done(err);
 
-					should.exist(resp);
-					resp.length.should.be.above(30000);
-					done();
-				});
-		});
-	});
+                            should.exist(r);
+                            should.exist(r.id);
+                            r.id.should.equal(meta.id);
+
+                            return done();
+                        });
+                },
+                done);
+        });
+    });
+
+    describe('getCatalogueItemByKey', function() {
+        var instance;
+        var sampleMetadata = [];
+
+        beforeEach(function(done) {
+            instance = new gutenberg();
+            instance.getCatalogueMetadata({
+                    maxImported: 10
+                },
+                function(err, keysResp) {
+                    if (err)
+                        return done(err);
+                    sampleMetadata = keysResp;
+                    return done();
+                });
+        });
+
+        it('Standard Success - Fixed - Above 10000', function(done) {
+            this.timeout(999999);
+            instance = new gutenberg();
+            instance.getCatalogueItemByKey({
+                    id: 10005,
+                    extension: 'txt'
+                },
+                function(err, contentResp) {
+                    if (err)
+                        return done(err);
+
+                    should.exist(contentResp.header);
+                    should.exist(contentResp.footer);
+                    should.exist(contentResp.content);
+
+                    return done();
+                });
+        });
+
+        it('Standard Success - Fixed - Below 10000', function(done) {
+            this.timeout(999999);
+            instance = new gutenberg();
+            instance.getCatalogueItemByKey({
+                    id: 9999,
+                    extension: 'txt'
+                },
+                function(err, contentResp) {
+                    if (err)
+                        return done(err);
+
+                    should.exist(contentResp.header);
+                    should.exist(contentResp.footer);
+                    should.exist(contentResp.content);
+
+                    return done();
+                });
+        });
+
+        it('Standard Success - Dynamic', function(done) {
+            this.timeout(999999);
+
+            async.eachSeries(sampleMetadata,
+                function(meta, metaCb) {
+                    instance.getCatalogueItemByKey({
+                            id: meta.id,
+                            extension: 'txt'
+                        },
+                        function(err, contentResp) {
+                            if (err)
+                                return metaCb(err);
+
+                            should.exist(contentResp.header);
+                            should.exist(contentResp.footer);
+                            should.exist(contentResp.content);
+
+                            return metaCb();
+                        });
+                },
+                done);
+        });
+    });
 });
