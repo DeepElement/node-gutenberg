23d0c49f2ddf5a0849d3908a34fbe287d5fd839d
Todd Morrison <todd@deepelement.com>
2014-10-09 13:20:49 -0700

    added test timer to prevent travis-ci timeouts

--- test/integration-gutenberg-tests.js
+++ test/integration-gutenberg-tests.js
@@ -9,6 +9,23 @@ var util = require('./test'),
 var maxImport = 2;
 
 describe('api integration', function() {
+
+    var loggingTimerId = null;
+    beforeEach(function(done) {
+        if (!loggingTimerId) {
+            loggingTimerId = setInterval(function(){
+                console.log('...');
+            }, 2000);
+        }
+    });
+
+    afterEach(function(done) {
+        if (loggingTimerId) {
+            clearInterval(loggingTimerId);
+            loggingTimerId = null;
+        }
+    });
+
     describe('getCatalogueMetadata', function() {
         var instance;
         var sampleKeys = [];
